59c2320e9eb325a09c35a3808f2b9776adb243f5
Anton Yalovoy <93antonyalovoy93@gmail.com>
2025-05-16 12:07:46 +0300

    Добавил двоеточие к тексту рекорда, добавил кубок : 0

--- Assets/Skript/ScoreManager.cs
+++ Assets/Skript/ScoreManager.cs
@@ -1,17 +1,18 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public int score = 0;
     public TMP_Text scoreText;
-
-    public TimerManager timerManager; // ссылка на таймер
+    public TMP_Text recordText;         // Просто число без слова "RECORD"
+    private int record = 0;
+    public TimerManager timerManager;
 
     void Start()
     {
         UpdateScoreUI();
+        UpdateRecordUI(); // Покажет 0 в начале
     }
 
     public void AddPoints(int amount)
@@ -19,14 +20,25 @@ public class ScoreManager : MonoBehaviour
         score += amount;
         UpdateScoreUI();
 
+        if (score > record)
+        {
+            record = score;
+            UpdateRecordUI();
+        }
+
         if (timerManager != null)
         {
-            timerManager.AddTime(2f); // добавляем 2 секунды
+            timerManager.AddTime(2f);
         }
     }
 
     void UpdateScoreUI()
     {
         scoreText.text = "Score: " + score.ToString();
+    }
+
+    void UpdateRecordUI()
+    {
+       recordText.text = ": " + record.ToString();
     }
 }
