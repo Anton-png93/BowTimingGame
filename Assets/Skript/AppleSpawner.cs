b2d7820f9cafa45f9161672b306c34b3bc8a919a
Anton Yalovoy <93antonyalovoy93@gmail.com>
2025-05-19 11:08:40 +0300

    🍏 Включено вращение яблок

---
+++ Assets/Skript/AppleSpawner.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class AppleSpawner : MonoBehaviour
+{
+    public GameObject applePrefab;      // Префаб яблока
+    public float spawnXMin = -2.5f;     // Левая граница появления
+    public float spawnXMax = 2.5f;      // Правая граница появления
+
+    private float timer;
+    private float nextSpawnTime;
+    private bool isSpawning = false;
+
+    void Update()
+    {
+        if (!isSpawning && GameManager.instance.GetScore() >= 80)
+        {
+            isSpawning = true;
+            SetNextSpawnTime();
+        }
+
+        if (!isSpawning) return;
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            SpawnApple();
+            SetNextSpawnTime();
+        }
+    }
+
+    void SetNextSpawnTime()
+    {
+        timer = Random.Range(5f, 8f); // Случайное число от 5 до 8 секунд
+    }
+
+    void SpawnApple()
+    {
+        float x = Random.Range(spawnXMin, spawnXMax);
+        Vector3 spawnPosition = new Vector3(x, transform.position.y, 0);
+        Instantiate(applePrefab, spawnPosition, Quaternion.identity);
+    }
+}
